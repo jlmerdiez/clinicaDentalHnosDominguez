2d4afaa2abc67e11757223e0e10cd15a74a0d681
José Luis Merino Diez <jlmerdiez@gmail.com>
2017-10-23 12:26:23 +0200

    Fichero PHP de inicio para el blog.

---
+++ blog/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Blog de la Clínica Dental Hermanos Domínguez Aramendi</title>
+</head>
+<body>
+	<?php echo '<p>Aquí va a implementarse un blog.</p>'; ?>
+</body>
+</html>
